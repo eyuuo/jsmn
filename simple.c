df5288e524fe151fe6280169b3da5f9a4137ccfa
eyuuo <21700549@handong.edu>
2018-04-11 02:45:35 +0900

    change simple.c

--- simple.c
+++ simple.c
@@ -25,9 +25,9 @@ int main() {
 	int r;
 	jsmn_parser p;
 	jsmntok_t t[128]; /* We expect no more than 128 tokens */
-	printf("===========");
+	printf("============");
 	printf("%s",JSON_STRING);
-        printf("===========");
+        printf("============");
 
 	jsmn_init(&p);
 	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
